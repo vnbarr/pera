73a65fb1b6e2f57883338cd136631f346cc8c433
Kleer LA <kleer@kleer-sandbox.(none)>
2016-04-29 13:02:53 -0300

    refactor a nombre en ingles de las clases y aniadimos la clase grid

--- lib/boat.rb
+++ lib/boat.rb
@@ -1,7 +1,8 @@
-class Barco
+class Boat
 	def initialize
 		@width = 1
 		@height = 1
+		@floating = true
 	end
 	
 	def get_width
@@ -11,4 +12,13 @@ class Barco
 	def get_height
 		return @height
 	end
+
+	def is_floating
+		return @floating
+	end
+	
+	def receive_bomb
+		@floating=false
+	end
+
 end
